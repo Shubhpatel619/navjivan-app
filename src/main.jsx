e81e328b6c47fc8b83232e66d71cd64ee29935cb
pshubh24 <2202030400125@silveroakuni.ac.in>
2026-03-15 18:41:06 +0530

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/navjivan-app">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter basename="/navjivan-app">
+    <App />
+  </BrowserRouter>
 )
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/navjivan-app">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// )
